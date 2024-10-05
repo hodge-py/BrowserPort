ee78d3a4db3a66dd9f1fb3cf001db86dd77bd656
hodge-py <105604814+hodge-py@users.noreply.github.com>
2024-10-05 15:44:16 -0600

    c

---
+++ script.js
@@ -0,0 +1,23 @@
+$(function(){
+    const { createApp, ref } = Vue
+
+    createApp({
+        setup() {
+            const message = ref('Hello vue!')
+            return {
+                message
+            }
+        }
+    }).mount('#app')
+
+
+
+
+
+
+
+
+
+
+
+});
